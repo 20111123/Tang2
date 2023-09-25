fd7111f65738ed90ab13f8b4bf57a89c7c081169
20111123 <144865643+20111123@users.noreply.github.com>
2023-09-24 23:56:13 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+function img01 () {
+    document.getElementById("trocarimg").src="img/S_Abacaxi2.png";
+}
+
+function img02 () {
+    document.getElementById("trocarimg").src="img/TA_abacaxi.png";
+}
