e17428a8689c5ef4b98fa26a2948c7a44d6f26a7
20111123 <144865643+20111123@users.noreply.github.com>
2023-09-29 21:17:26 -0300

    Update 4 script2.js

--- script2.js
+++ script2.js
@@ -5,3 +5,19 @@ function img01 () {
 function img02 () {
     document.getElementById("trocarimg").src="img/TA_abacaxi.png";
 }
+
+function img03 () {
+    document.getElementById("trocarimg").src="img/S_guarana2.png";
+}
+
+function img04 () {
+    document.getElementById("trocarimg").src="img/TA_guarana.png";
+}
+
+function img05 () {
+    document.getElementById("trocarimg").src="img/S_Goiaba2.png";
+}
+
+function img06 () {
+    document.getElementById("trocarimg").src="img/TA_goiaba.png";
+}
